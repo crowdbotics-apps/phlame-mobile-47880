9bcf921ef88d4a0a36ed3bd4a415d10f6fac7b2b
Crowdbotics <team@crowdbotics.com>
2024-05-12 02:02:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "phlame-mobile-47880",
+  url: "https://phlame-mobile-47880.botics.co",
+  api: "https://phlame-mobile-47880.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
